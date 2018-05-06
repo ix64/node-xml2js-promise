e7d7269558b5c3702e8a6dbc7cf85b956119e89f
ix64-home <admin@ixarea.com>
2018-05-06 13:51:50 +0800

    add all

---
+++ test.js
@@ -0,0 +1,16 @@
+const xml2js_promise = require('./xml2js-promise.js');
+const should = require('should');
+const valid_xml = "<?xml version=\"1.0\" encoding=\"utf-8\" ?><xml>is valid</xml>";
+const invalid_xml = "<?xml version=\"1.0\" encoding=\"utf-8\" ?><xml>is</invalid>";
+
+describe("parse xml", () => {
+        it("valid xml", async () => {
+            let result = await  xml2js_promise(valid_xml);
+            result.xml.should.equal('is valid');
+        });
+        it("invalid xml", async () => {
+            let result = await  xml2js_promise(invalid_xml);
+            should(result).undefined();
+        });
+    }
+);
---
+++ xml2js-promise.js
@@ -0,0 +1,17 @@
+const xml2js = require('xml2js');
+
+module.exports = (xmlString, options) => {
+    return new Promise((resolve, reject) => {
+        xml2js.parseString(xmlString, options, (err, result) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(result);
+            }
+        })
+    }).catch(() => {
+        return undefined;
+    });
+};
+
+
